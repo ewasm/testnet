d507ba5c319bd3adb2559a5cd8bc4111894e097e
cdetrio <cdetrio@gmail.com>
2018-06-16 13:48:59 -0400

    fix peerset flag when empty

--- cpp-eth.sh
+++ cpp-eth.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-echo "printing versions.."
-# cat /cpp-ethereum-version.json
-# cat /hera-version.json
 
 PEER_1="enode://a604159cad54b3136812fd19c1656a035f6070e27fc3ab433bc04b61fb2c1a27f6563a53b80c88e2453b6b059bd7658e59ebbbb60de8d09ae9b78fce8e84887b@miner1:30303"
 PEER_2="enode://eb085e5795760a7aa83026b56c433ba3b027738eed2cbf4ebad6ccf0ca3ccd78369f4ffa77b59f763e31a41245980d246c2e0acb6362785a9ec21824385cf87b@miner2:30303"
@@ -34,18 +31,26 @@ fi
 echo $PEER_SET
 
 echo "running eth..."
-eth \
-  --vm /libhera.so \
-  --evmc fallback=true  \
-  --db-path /tmp/ewasm-node/4201 \
-  --no-bootstrap \
-  --mining on \
-  --mining-threads 1 \
-  --ask 1 \
-  --address 0x031159dF845ADe415202e6DA299223cb640B9DB0 \
-  --config /ewasm-testnet-cpp-config.json \
-  --listen 1234 \
-  --peerset "$PEER_SET" &
+
+
+ethCmd=(eth)
+
+ethCmd+=(--vm /libhera.so)
+ethCmd+=(--evmc fallback=true)
+ethCmd+=(--db-path /tmp/ewasm-node/4201)
+ethCmd+=(--no-bootstrap)
+ethCmd+=(--mining on)
+ethCmd+=(--mining-threads 1)
+ethCmd+=(--ask 1)
+ethCmd+=(--address 0x031159dF845ADe415202e6DA299223cb640B9DB0)
+ethCmd+=(--config /ewasm-testnet-cpp-config.json)
+ethCmd+=(--listen 1234)
+ethCmd+=(${PEER_SET:+ --peerset "${PEER_SET}"}) # only use --peerset when $PEER_SET not empty
+
+echo ${ethCmd[@]}
+
+"${ethCmd[@]}" &
+  
 
 #  --peerset "required:61e5475e6870260af84bcf61c02b2127a5c84560401452ae9c99b9ff4f0f343d65c9e26209ec32d42028b365addba27824669eb70c73f69568964f77433afbbe@127.0.0.1:1234" \
 
